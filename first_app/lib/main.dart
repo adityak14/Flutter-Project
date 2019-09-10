0dd927d6297aab1667ebc4a210becd539165db32
adityak14 <aditya.kaushik1213@gmail.com>
2019-09-10 08:13:16 +0530

    Final app

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -12,7 +12,7 @@ void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Flutter Project';
 
   @override
   Widget build(BuildContext context) {
